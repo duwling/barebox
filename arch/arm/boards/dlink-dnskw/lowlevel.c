8343f47521371816b26571658234b0784f4dd030
David Unterwandling <dj@root.priv.at>
2022-11-27 11:18:58 +0100

    ARM: Kirkwood: add D-Link DNS-32x board support

---
+++ arch/arm/boards/dlink-dnskw/lowlevel.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <common.h>
+#include <linux/sizes.h>
+#include <asm/barebox-arm.h>
+#include <mach/mvebu/barebox-arm-head.h>
+#include <mach/mvebu/lowlevel.h>
+
+#define kw_entry_(model)					\
+	ENTRY_FUNCTION_MVEBU(start_dlink_##model, r0, r1, r2)	\
+	{							\
+		extern char __dtb_kirkwood_##model##_start[];	\
+		void *fdt;					\
+								\
+		arm_cpu_lowlevel_init();			\
+								\
+		fdt = __dtb_kirkwood_##model##_start		\
+			+ get_runtime_offset();			\
+								\
+		kirkwood_barebox_entry(fdt);			\
+	}
+kw_entry_(dns325);
+kw_entry_(dns320);
+kw_entry_(dns320l);
+kw_entry_(dns320b);
